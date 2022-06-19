d98004d8010458505fc1d8bfe1155f6b625e65ca
Sandip Shengole <sandip.shengole@gmail.com>
2022-06-19 21:05:59 +0530

    applicatoin layout using bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/component/login/login.component';
+import { RegistrationComponent } from './modules/component/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/component/login/login.component';
+import { RegistrationComponent } from './modules/component/registration/registration.component';
+import { FooterComponent } from './modules/layout/footer/footer.component';
+import { HeaderComponent } from './modules/layout/header/header.component';
+import { SidebarComponent } from './modules/layout/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrationComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
